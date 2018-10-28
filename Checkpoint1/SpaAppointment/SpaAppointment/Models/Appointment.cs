390496d69ee558e63c7e4d7b1a9ec557977028e8
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-10-27 23:06:17 -0500

    Got my Checkpoint1 app started

---
+++ Checkpoint1/SpaAppointment/SpaAppointment/Models/Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaAppointment.Models
+{
+    public class Appointment
+    {
+        public DateTime appDate { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ Checkpoint1/SpaAppointment/SpaAppointment/Models/ServiceProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaAppointment.Models
+{
+    public class ServiceProvider
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+        public Customer thisServiceProvidersCustomer { get; set; }
+    }
+}
