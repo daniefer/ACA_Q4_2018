bf83e72f1cae8c6d9e98720e71759883d5cd2622
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-10-27 23:59:09 -0500

    added my repository

--- Checkpoint1/SpaAppointment/SpaAppointment/Models/Appointment.cs
+++ Checkpoint1/SpaAppointment/SpaAppointment/Models/Appointment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace SpaAppointment.Models
 {
     public class Appointment
     {
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy h:mm tt")]
         public DateTime appDate { get; set; }
         public int id { get; set; }
     }
---
+++ Checkpoint1/SpaAppointment/SpaAppointment/Services/Repository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaAppointment.Services
+{
+    public class Repository
+    {
+    }
+}
